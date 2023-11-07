40ee1bbb0ce4b97da224121d5ef9a5fca4c7a30c
Nafiul <nafiul.arpon@gmail.com>
2023-11-07 17:04:23 +0600

    up

--- localization.py
+++ localization.py
@@ -9,13 +9,12 @@ from scipy.spatial import distance
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-
 custom_dtype = np.dtype([
     ('hit', np.int8),       
     ('accuracy', np.int8),
     ('class', np.int8)       
 ])
-map = np.zeros((1000, 1000, 1000), dtype=custom_dtype)
+
 
 def max_hit(points):
     final_list = []
@@ -37,79 +36,81 @@ model = YOLO('yolov8n.pt')
 dc = DepthCamera()
 hit_map = np.zeros((1000,1000))
 detect_list = [39,41,-99]
-robot = (500,500,0)
-map[robot]['hit'] = 100
-
-while True:
-    
-    ret, depth_frame, color_frame, depth_info = dc.get_frame()
-    img = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
-    results = model.predict(img)
-    for r in results:
-        
-        annotator = Annotator(color_frame)
-        
-        boxes = r.boxes
-        for box in boxes:
-            
-            b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
-            c = box.cls
-            pt1 = (int(b[0].detach().cpu().numpy()),int(b[1].detach().cpu().numpy()))
-            x = box.xywh[0][0].detach().cpu().numpy()
-            y = box.xywh[0][1].detach().cpu().numpy()       
-            point = int(x), int(y)
-            if c in detect_list:
-                if cv2.waitKey(1) & 0xFF == ord('m'):
-                    x = int(input('X: '))
-                    y = int(input('Y: '))
-                    z = int(input('Z: '))
-                    map[robot]['hit'] = 0
-                    robot = (x,y,z) 
-                    map[robot]['hit'] = 100
-                           
-                points = dc.Global_points(point[0],point[1])
-                points[0][1] *= -1
-                dx = 500+points[0][0]*100
-                dy = 500+points[0][1]*100
-                map[round(dx),round(dy),round(points[0][2]*100)]['hit'] += 1
-                map[round(dx),round(dy),round(points[0][2]*100)]['class'] = c
-                cv2.circle(color_frame, point, 4, (0, 0, 255))
-                annotator.box_label(b, model.names[int(c)]+" x:"+str(round(points[0][0],2))+" y:"+str(round(points[0][1],2))+" z:"+str(round(points[0][2],2)))
-             
-
-    color_frame = annotator.result()  
-    cv2.imshow('Detection', color_frame)     
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        break  
-
-indices = np.where(map['hit'] > 1)
-
-points = list(zip(indices[0], indices[1], indices[2]))
-points = max_hit(points)
-
 datax = []
-for i, point in enumerate(points):
-    datax.append([point[0]-500,point[1]-500,point[2],map[point]['hit']])
+while True:
+    map = np.zeros((1000, 1000, 1000), dtype=custom_dtype)
+    while True: 
+        ret, depth_frame, color_frame, depth_info = dc.get_frame()
+        img = cv2.cvtColor(color_frame, cv2.COLOR_BGR2RGB)
+        results = model.predict(img)
+        for r in results:
+            
+            annotator = Annotator(color_frame)
+            
+            boxes = r.boxes
+            for box in boxes:
+                
+                b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
+                c = box.cls
+                pt1 = (int(b[0].detach().cpu().numpy()),int(b[1].detach().cpu().numpy()))
+                x = box.xywh[0][0].detach().cpu().numpy()
+                y = box.xywh[0][1].detach().cpu().numpy()       
+                point = int(x), int(y)
+                if c in detect_list:
+                    if cv2.waitKey(1) & 0xFF == ord('m'):
+                        x = int(input('X: '))
+                        y = int(input('Y: '))
+                        z = int(input('Z: '))
+                        map[robot]['hit'] = 0
+                        robot = (x,y,z) 
+                        map[robot]['hit'] = 100
+                            
+                    points = dc.Global_points(point[0],point[1])
+                    points[0][1] *= -1
+                    dx = 500+points[0][0]*100
+                    dy = 500+points[0][1]*100
+                    map[round(dx),round(dy),round(points[0][2]*100)]['hit'] += 1
+                    map[round(dx),round(dy),round(points[0][2]*100)]['class'] = c
+                    cv2.circle(color_frame, point, 4, (0, 0, 255))
+                    annotator.box_label(b, model.names[int(c)]+" x:"+str(round(points[0][0],2))+" y:"+str(round(points[0][1],2))+" z:"+str(round(points[0][2],2)))
+                
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-xs, ys, zs, hits = zip(*datax)
-ax.scatter(xs, ys, zs, s=hits)
-plt.show()
-# with open('output.csv', 'w', newline='') as file:
-#     writer = csv.writer(file)
-#     for row in data:
-#         writer.writerow(row)
+        color_frame = annotator.result()  
+        cv2.imshow('Detection', color_frame)     
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break  
 
-translation = (1, 2, 3)  
-axis = (0, 1, 0)         
-angle = np.radians(45)   
+    map[500,500,0]['hit'] = 0
+    indices = np.where(map['hit'] > 1)
+    points = list(zip(indices[0], indices[1], indices[2]))
+    points = max_hit(points)
+    print(points)
+    for i, point in enumerate(points):
+        datax.append([point[0]-500,point[1]-500,point[2],map[point]['hit']])
 
-rotation_matrix = axangle2aff(axis, angle)
-affine_matrix = compose(T=translation, R=rotation_matrix[:3, :3], Z=np.ones(3))
+    if input() == 'x':
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        xs, ys, zs, hits = zip(*datax)
+        ax.scatter(xs, ys, zs, s=hits)
+        plt.show()
+        
+        x1, y1, z1, h1= datax[0]
+        x2, y2, z2, h2 = datax[1]
+        translation = (-x2, -y2, -z2)  
+        axis = (0, 1, 0)         
+        angle = np.radians(0)   
 
-point = np.array([4, 5, 6, 1])
-transformed_point = affine_matrix.dot(point)
+        rotation_matrix = axangle2aff(axis, angle)
+        affine_matrix = compose(T=translation, R=rotation_matrix[:3, :3], Z=np.ones(3))
 
-print(transformed_point[:3])
+        point = np.array([x1, y1, z1, 1])
+        transformed_point = affine_matrix.dot(point)
+
+        print(transformed_point[:3])
+        break
+    else:
+        continue
+
+
